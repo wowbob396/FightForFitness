13cc557b9ad79b2de106b17c1d7343dc050a14b4
Brandon Ly <wowbob396@gmail.com>
2019-04-02 13:57:03 -0500

    updated test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fight_for_fitness/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FightApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
