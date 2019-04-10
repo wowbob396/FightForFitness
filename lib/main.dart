8713c5ee4b45b9b81f7d8573f6696695b7f750a2
Brandon Ly <wowbob396@gmail.com>
2019-04-09 21:53:08 -0500

    updated custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,38 @@ void main() => runApp(FightApp());
 class FightApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return MaterialApp(
+      title: "Fight For Fitness",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fight For Fitness"),
+
+        ),
+        body: Center(
+          child: Text("Test"),
+        ),
+      ),
+    );
   }
+}
+
+Widget buttonSection() {
+  Color color = Colors.blue;
+  print("Hello, World");
+  Widget buttonSection = Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+
+      ],
+    ),
+  );
+
+  return buttonSection;
+}
+
+Widget buildButtonColumn(Color color, IconData icon, String label) {
+  return Column (
+
+  );
 }
