c27fae890ac5d8de7fd02bda20a904e9782ebc5e
Brandon Ly <wowbob396@gmail.com>
2019-04-11 13:35:07 -0500

    added bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(FightApp());
 
-class FightApp extends StatelessWidget {
+class FightApp extends StatefulWidget {
+  FightApp({Key key}) : super(key: key);
+
+  @override
+  _FightAppState createState() => _FightAppState();
+}
+/// Main app class
+class _FightAppState extends State<FightApp> {
+    int _selectedIndex = 0;
+
+    final _widgetOptions = [
+      Text('Classes'),
+      Text('Calendar'),
+      Text('About'),
+      Text('Contact'),
+    ];
+
     @override
     Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,9 +31,30 @@ class FightApp extends StatelessWidget {
         body: Center(
           child: Text("Test"),
         ),
+        bottomNavigationBar: BottomNavigationBar(items:
+          <BottomNavigationBarItem>  [
+            BottomNavigationBarItem(icon: Icon(Icons.filter_hdr),title: Text("Classes")),
+            BottomNavigationBarItem(icon: Icon(Icons.calendar_today), title: Text("Calendar")),
+            //BottomNavigationBarItem(icon: Icon(Icons.info), title: Text("About")),
+            BottomNavigationBarItem(icon: Icon(Icons.contact_mail), title: Text("Contact")),
+          ],
+          currentIndex: _selectedIndex,
+          fixedColor: Colors.green,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
+
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+}
+
+Widget _customNavBar() {
+
 }
 
 Widget buttonSection() {
@@ -35,8 +72,12 @@ Widget buttonSection() {
   return buttonSection;
 }
 
+
+
 Widget buildButtonColumn(Color color, IconData icon, String label) {
   return Column (
 
   );
+
+
 }
