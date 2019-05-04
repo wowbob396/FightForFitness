beae9fa28dc7e559ae04d1f9fd568d48b1da9779
wowbob396 <wowbob396@gmail.com>
2019-05-04 13:21:03 -0500

    added build list

--- lib/components/AnnouncementPage.dart
+++ lib/components/AnnouncementPage.dart
@@ -28,12 +28,31 @@ class AnnouncementPage extends StatelessWidget {
     );
   }
 
-  Widget _buildList() {
-
+  Widget _buildList(BuildContext context, List<Map> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
   }
 
   Widget _buildListItem(BuildContext context, Map data) {
     final Announcement announcement = Announcement.fromMap(data);
+
+    return Padding(
+      key: ValueKey(announcement.date),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(announcement.text),
+          trailing: Text(announcement.date.toIso8601String()),
+          onTap: () => print(announcement)
+        ),
+      ),
+    );
   }
 }
 
@@ -49,4 +68,7 @@ class Announcement {
       date = map['date'];
 
   Announcement.fromSnapshot(DocumentSnapshot snapshot): this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Announcement<$date:$text>";
 }
