5bbee5f09479d7f94b1ba0d0ae93fea9e1931dad
wowbob396 <wowbob396@gmail.com>
2019-05-06 22:22:51 -0500

    started working on contact page

--- lib/components/ContactPage.dart
+++ lib/components/ContactPage.dart
@@ -5,7 +5,34 @@ class ContactPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Text("Hello, World!  Welcome to the contact page"),
+      body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            children: <Widget>[
+              Text("Hello"),
+              SizedBox(height: 120),
+              Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: 'Username',
+                    ),
+                  ),
+// spacer
+                  SizedBox(height: 12.0),
+// [Password]
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: 'Email',
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+      ),
     );
   }
 }
