52d9b5efcce9a97d29f1b460c5ed5a431c219071
wowbob396 <wowbob396@gmail.com>
2019-05-25 21:01:07 -0500

    refactoring calendar page

--- lib/components/CalendarPage.dart
+++ lib/components/CalendarPage.dart
@@ -3,84 +3,42 @@ import 'package:flutter/material.dart';
 /// The CalendarPage class contains a stateless widget that contains
 /// information about the weekly class schedule
 class CalendarPage extends StatelessWidget {
+
+  DateTime _date = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(16.0),
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0/11.0,
-                  child: Image.asset("assets/mma.png"),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text("MMA Sparring"),
-                      SizedBox(height: 8.0),
-                      Text("7:15PM - 8:15PM"),
-                    ],
+
+    return Row(
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text("")
+                ],
+              ),
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: Colors.black,
                   ),
-                )
-              ],
-            ),
-          ),
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0/11.0,
-                  child: Image.asset("assets/grappling.png"),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text("Grappling"),
-                      SizedBox(height: 8.0),
-                      Text("8:15AM - 9:15AM"),
-                    ],
+                  bottom: BorderSide(
+                    color: Colors.black,
                   ),
-                )
-              ],
-            ),
-          ),
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0/11.0,
-                  child: Image.asset("assets/striking.png"),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text("Striking"),
-                      SizedBox(height: 8.0),
-                      Text("6:10PM - 7:10PM"),
-                    ],
+                  left: BorderSide(
+                    color: Colors.black,
                   ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
+                  right: BorderSide(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 
